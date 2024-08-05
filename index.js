d139e004014b0238f5aeefc698c58312670d78b6
Carson Rivera <carsonrivera04@gmail.com>
2024-08-05 15:52:45 -0400

    Added animation

---
+++ index.js
@@ -0,0 +1,97 @@
+function myMove1(callback) {
+  let id = null;
+  const elem = document.getElementById("animate");   
+  elem.style.display = "block"; 
+  let xpos = 0, ypos = 0; 
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (xpos == 50 && ypos == -100) {
+      clearInterval(id);
+      elem.style.right = 0; 
+      elem.style.bottom = 0; 
+      if (callback) callback(); 
+    } else {
+      if (xpos < 50) xpos += 1; 
+      if (ypos > -100) ypos -= 1; 
+      elem.style.right = xpos + "%"; 
+      elem.style.bottom = ypos + "%"; 
+    }
+  }
+}
+
+function myMove2(callback) {
+  let id = null;
+  const elem = document.getElementById("animate");   
+  let xpos = 0, ypos = 0; 
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (xpos == 50 && ypos == 100) {
+      clearInterval(id);
+      elem.style.right = 0; 
+      elem.style.bottom = 0; 
+      if (callback) callback(); 
+    } else {
+      if (xpos < 50) xpos += 1; 
+      if (ypos < 100) ypos += 1; 
+      elem.style.right = xpos + "%"; 
+      elem.style.bottom = ypos + "%"; 
+    }
+  }
+}
+
+function myMove3(callback) {
+  let id = null;
+  const elem = document.getElementById("animate");   
+  let xpos = 0, ypos = 0; 
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  let temp = (window.innerWidth / 2) - 164;
+  function frame() {
+    if (xpos >= temp && ypos == -100) {
+      clearInterval(id);
+      elem.style.right = 0; 
+      elem.style.bottom = 0; 
+      if (callback) callback(); 
+    } else {
+      if (xpos < temp) xpos += 4; 
+      if (ypos > -100) ypos -= 1; 
+      elem.style.right = xpos + "px"; 
+      elem.style.bottom = ypos + "%"; 
+    }
+  }
+}
+
+function myMove4(callback) {
+  let id = null;
+  const elem = document.getElementById("animate");   
+  let xpos = 0, ypos = 0; 
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  let temp = (window.innerWidth / 2) - 164;
+  function frame() {
+    if (xpos >= temp && ypos == 100) {
+      clearInterval(id);
+      elem.style.right = 0; 
+      elem.style.bottom = 0; 
+      elem.style.display = "none"; 
+      if (callback) callback(); 
+    } else {
+      if (xpos < temp) xpos += 4; 
+      if (ypos < 100) ypos += 1; 
+      elem.style.right = xpos + "px"; 
+      elem.style.bottom = ypos + "%"; 
+    }
+  }
+}
+
+function myMove() {
+  myMove1(() => {
+    myMove2(() => {
+      myMove3(() => {
+        myMove4();
+      });
+    });
+  });
+}
