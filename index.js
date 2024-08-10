1ddfe3a0394deaca9bf9712334994857ea155958
Carson Rivera <carsonrivera04@gmail.com>
2024-08-09 21:23:47 -0400

    Added functionality for all buttons + game loop

--- index.js
+++ index.js
@@ -3,29 +3,6 @@ class Card {
     this.rank = rank; 
     this.suit = suit; 
   }
-  cardCount() {
-    switch(this.rank) {
-      case 2: 
-      case 3: 
-      case 4: 
-      case 5: 
-      case 6: 
-        return 1; 
-        break; 
-      case 7:
-      case 8:
-      case 9:
-        return 0; 
-        break; 
-      case 10: 
-      case 11: 
-      case 12: 
-      case 13: 
-      case 1: 
-        return -1; 
-        break; 
-    }
-  }
 }
 
 const deck = []; 
@@ -47,6 +24,10 @@ for (let i = 0; i < 4; i++) {
     }
   }
 }
+Shuffle(deck); 
+
+let hp = { key: 0 }; 
+let hw = { key: 0 }; 
 
 function Deal() {
   for (let i = 1; i <= 4; i++) {
@@ -54,10 +35,19 @@ function Deal() {
     document.getElementById(str).style.visibility = "hidden"; 
   }
   myMove(); 
+  document.getElementById("hit").style.display = "block"; 
+  document.getElementById("stand").style.display = "block"; 
+  document.getElementById("double").style.display = "block"; 
+  document.getElementById("split").style.display = "block"; 
+  
+  document.getElementsByClassName("panel")[0].style.display = "none"; 
+  document.getElementsByClassName("panel")[1].style.display = "none"; 
+
   for (let i = 1; i <= 3; i++) {
-    let str = "card" + i.toString(); 
-    let elem = document.getElementById(str);
-    let rank = deck.pop().rank; 
+    const str = "card" + i.toString(); 
+    const elem = document.getElementById(str);
+    const card = deck.pop(); 
+    const rank = card.rank; 
     switch(rank) {
       case 1: 
         elem.getElementsByClassName("rank")[0].innerHTML = 'A'; 
@@ -80,11 +70,80 @@ function Deal() {
         elem.getElementsByClassName("rank")[1].innerHTML = rank; 
         break; 
     }
+    const suit = "./icons/" + card.suit + ".png"; 
+    elem.getElementsByClassName("suit")[0].src = suit; 
+    elem.getElementsByClassName("suit")[1].src = suit; 
+    elem.getElementsByClassName("suit-image")[0].src = suit; 
+    if (card.suit == "spades" || card.suit == "clubs") {
+      elem.getElementsByClassName("rank")[0].style.color = "black"
+      elem.getElementsByClassName("rank")[1].style.color = "black"
+    }
+    else {
+      elem.getElementsByClassName("rank")[0].style.color = "rgb(200, 42, 61)" 
+      elem.getElementsByClassName("rank")[1].style.color = "rgb(200, 42, 61)" 
+    }
   }
 }
 
-function Hit() {
-  
+function PlayerChoice(choice, hp, hw) {
+  let dcard = document.getElementById("card2").getElementsByTagName("p")[0].innerHTML;
+  switch(dcard) {
+    case 'A':
+      dcard = 1; 
+      break; 
+    case 'J': 
+    case 'Q': 
+    case 'K': 
+      dcard = 10; 
+      break; 
+    default: 
+      dcard = Number(dcard);
+  }
+  let pcard1 = document.getElementById("card1").getElementsByTagName("p")[0].innerHTML;
+  switch(pcard1) {
+    case 'A':
+      pcard1 = 1; 
+      break; 
+    case 'J': 
+    case 'Q': 
+    case 'K': 
+      pcard1 = 10; 
+      break; 
+    default: 
+      pcard1 = Number(pcard1);
+  }
+  let pcard2 = document.getElementById("card3").getElementsByTagName("p")[0].innerHTML;
+  switch(pcard2) {
+    case 'A':
+      pcard2 = 1; 
+      break; 
+    case 'J': 
+    case 'Q': 
+    case 'K': 
+      pcard2 = 10; 
+      break; 
+    default: 
+      pcard2 = Number(pcard2);
+  }
+  document.getElementById("hit").style.display = "none"; 
+  document.getElementById("stand").style.display = "none"; 
+  document.getElementById("double").style.display = "none"; 
+  document.getElementById("split").style.display = "none"; 
+  const correct = CorrectMove(dcard, pcard1, pcard2); 
+  document.getElementById("rightwrong").style.display = "block"; 
+  const elem1 = document.getElementById("rightwrong");
+  hp.key++; 
+  if (correct == choice) {
+    hw.key++; 
+    elem1.innerHTML = "CORRECT, player was supposed to " + correct; 
+    elem1.style.backgroundColor = "green"; 
+  }
+  else {
+    elem1.innerHTML = "WRONG, player was supposed to " + correct; 
+    elem1.style.backgroundColor = "red"; 
+  }
+  document.getElementById("stats").style.display = "block";
+  document.getElementById("stats").innerHTML = "You have played " + hw.key + '/' + hp.key + " correctly!";
 }
 
 // ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
@@ -190,5 +249,148 @@ function myMove() {
   });
 }
 
+function CorrectMove(dcard, pcard1, pcard2) {
+  if (dcard == 11 || dcard == 12 || dcard == 13) 
+    dcard = 10; 
+  if (pcard1 == 11 || pcard1 == 12 || pcard1 == 13) 
+    pcard1 = 10; 
+  if (pcard2 == 11 || pcard2 == 12 || pcard2 == 13) 
+    pcard2 = 10; 
 
+  // correct move for pairs 
+  if (pcard1 == pcard2) {
+    switch(pcard1) {
+      case 1: 
+      case 8: 
+        return "split"; 
+        break; 
+      case 2: 
+      case 3: 
+        if (dcard >= 4 && dcard <= 7) 
+          return "split"; 
+        else 
+          return "hit"; 
+        break; 
+      case 4:
+        return "hit";
+        break; 
+      case 5: 
+        if (dcard == 1 || dcard == 10)
+          return "hit"; 
+        else 
+          return "double"; 
+        break; 
+      case 6: 
+        if (dcard >= 3 && dcard <= 6)
+          return "split";
+        else 
+          return "hit"; 
+        break; 
+      case 7: 
+        if (dcard >= 2 && dcard <= 7)
+          return "split";
+        else 
+          return "hit"; 
+        break; 
+      case 9: 
+        if (dcard == 7 || dcard == 10 || dcard == 1) 
+          return "stand"; 
+        else 
+          return "split"; 
+        break; 
+      case 10:
+        return "stand"; 
+        break; 
+    }
+  }
+  else if (pcard1 == 1 || pcard2 == 1) {
+    switch (pcard1 + pcard2) {
+      case 3:
+      case 4:
+        if (dcard == 5 || dcard == 6) 
+          return "double"; 
+        else 
+          return "hit"; 
+        break; 
+      case 5: 
+      case 6: 
+        if (dcard >= 4 && dcard <= 6) 
+          return "double"; 
+        else 
+          return "hit"; 
+        break; 
+      case 7:
+        if (dcard >= 3 && dcard <= 6) 
+          return "double"; 
+        else 
+          return "hit"; 
+        break; 
+      case 8: 
+        if (dcard == 7 || dcard == 8 || dcard == 2)
+          return "stand"; 
+        else if (dcard >= 3 && dcard <= 6)
+          return "double"; 
+        else 
+          return "hit"; 
+        break; 
+      case 9:
+      case 10:
+      case 11:
+        return "stand"; 
+        break; 
+    }
+  }
+  else {
+    switch (pcard1 + pcard2) {
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+        return "hit"; 
+        break; 
+      case 9: 
+        if (dcard >= 2 && dcard <= 6)
+          return "double"; 
+        else 
+          return "hit"; 
+        break;
+      case 10: 
+        if (dcard == 10 || dcard == 1)
+          return "hit"; 
+        else 
+          return "double"; 
+        break; 
+      case 11:
+        if (dcard == 1)
+          return "hit";
+        else 
+          return "double"; 
+        break; 
+      case 12: 
+        if (dcard >= 4 && dcard <= 6) 
+          return "stand"; 
+        else 
+          return "hit"; 
+        break; 
+      case 13:
+      case 14:
+      case 15:
+      case 16:
+        if (dcard >= 2 && dcard <= 6) 
+          return "stand"; 
+        else 
+          return "hit"; 
+        break; 
+      default: 
+        return "stand"; 
+        break; 
+    }
+  }
+}
 
+function Shuffle(arr) {
+  for (let i = arr.length - 1; i > 0; i--){
+    let j = Math.floor(Math.random() * (i + 1)); 
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  } 
+} 
